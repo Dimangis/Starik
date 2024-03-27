bcb5c333f8b9b5bf877da56e33e5fc9b38e6fbcf
Diangis <nissan200oe@yandex.ru>
2024-03-27 15:34:14 +0300

    added section howWeWorks

---
+++ src/components/howWeWorks/howWeWorks.js
@@ -0,0 +1,25 @@
+document.write(
+  '    <div class="container">\
+<h2>Как мы работаем?</h2>\
+<ul class="howWeWorks-list">\
+  <li class="howWeWorks-item">\
+    <span class="howWeWorks-item_num">01</span>\
+    <p class="howWeWorks-item_description">\
+      Получаем заявку на коммерческое предложение\
+    </p>\
+  </li>\
+  <li class="howWeWorks-item">\
+    <span class="howWeWorks-item_num">02</span>\
+    <p class="howWeWorks-item_description">\
+      Получаем заявку на коммерческое предложение\
+    </p>\
+  </li>\
+  <li class="howWeWorks-item">\
+    <span class="howWeWorks-item_num">03</span>\
+    <p class="howWeWorks-item_description">\
+      Получаем заявку на коммерческое предложение\
+    </p>\
+  </li>\
+</ul>\
+</div>'
+);
